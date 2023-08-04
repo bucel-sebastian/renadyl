370ca783a51bd7738eda3a789a84c438e6133ce4
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2023-08-04 20:03:22 +0300

    loading styling

--- frontend/app/[locale]/loading.js
+++ frontend/app/[locale]/loading.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import renadylLogo from '../../public/renadyl_logo.svg'
 
 export default function Loading() {
-    return (<div className='fixed top-0 left-0 w-screen h-screen flex justify-center items-center'>
+    return (<div className='fixed top-0 left-0 bg-backgroundPrimary w-screen h-screen flex justify-center items-center z-50'>
     <Image
         src={renadylLogo}
         width={300}
