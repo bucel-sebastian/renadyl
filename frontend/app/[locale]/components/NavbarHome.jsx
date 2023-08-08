64cc43c9e3afa6781f2c8965965fd65bac21f83a
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2023-08-08 10:36:53 +0300

    optimized navbar logo

--- frontend/app/[locale]/components/NavbarHome.jsx
+++ frontend/app/[locale]/components/NavbarHome.jsx
@@ -39,7 +39,7 @@ function NavbarHome() {
     <header className={`fixed top-0 left-0 w-full py-2 px-5 px-5 z-50 duration-300 ${headerWhite ? 'bg-backgroundPrimary' : '' }`}>
       <div className='flex flex-row max-w-[1200px] justify-between content-center items-center w-full mx-auto'>
         <div>
-          <Link href="#" className='header-logo-container'>
+          <Link href="#" className='header-logo-container w-[200px] max-md:w-[150px] '>
             <Image 
               src={renadylLogo}
               alt='Renadyl'
