eea1363fdc3ed7f5d2e01345aa9e67e7ec27943b
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2023-09-22 02:24:50 +0300

    fixed build bugs

--- frontend/app/[locale]/(homepage)/loading.js
+++ frontend/app/[locale]/(homepage)/loading.js
@@ -1,15 +1,10 @@
-import renadylLogo from '@/public/renadyl_logo.svg';
+
 import Image from 'next/image';
 
 export default function Loading() {
     return (
         <div className="w-screen h-screen bg-backgroundPrimary flex justify-center items-center content-center">
-            <Image
-                src={renadylLogo}
-                width={200}
-                height={100}
-                className='w-[200px]'
-            />
+        
         </div>
     )
 }
