74290018f8b31b659ba44c1fcf2fc8df9a61208c
Bucel Sebastian <78745608+bucel-sebastian@users.noreply.github.com>
2023-09-26 12:24:26 +0000

    restore middleware

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -3,17 +3,17 @@ import createMiddleWare from 'next-intl/middleware';
 export default createMiddleWare({
     locales: ['ro','en','de'],
     defaultLocale: 'ro',
-    localeDetection: false,
-    domains: [
-        {
-            domain: 'renadyleurope.com',
-            defaultLocale: 'en',
-        },
-        {
-            domain: 'renadyl.ro',
-            defaultLocale: 'ro',
-        }
-    ]
+    localeDetection: false
+    // domains: [
+    //     {
+    //         domain: 'renadyleurope.com',
+    //         defaultLocale: 'en',
+    //     },
+    //     {
+    //         domain: 'renadyl.ro',
+    //         defaultLocale: 'ro',
+    //     }
+    // ]
 });
 
 export const config = {
