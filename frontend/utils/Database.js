0c4bd310c60a4da4ecdd80ff0840ac658896da87
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2023-10-16 01:29:31 +0300

    Update Database.js

--- frontend/utils/Database.js
+++ frontend/utils/Database.js
@@ -3,11 +3,11 @@ const { Pool } = require("pg");
 class Database {
   constructor() {
     this.pool = new Pool({
-      //   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-      host: process.env.POSTGRES_URL,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+      // host: process.env.POSTGRES_URL,
+      // user: process.env.POSTGRES_USER,
+      // password: process.env.POSTGRES_PASSWORD,
+      // database: process.env.POSTGRES_DATABASE,
     });
   }
 
