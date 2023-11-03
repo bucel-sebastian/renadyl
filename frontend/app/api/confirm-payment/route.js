f5328839bc751129dbbed20b09413ceeb1965e87
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2023-11-03 11:36:54 +0200

    Update route.js

--- frontend/app/api/confirm-payment/route.js
+++ frontend/app/api/confirm-payment/route.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 export async function POST(req, res) {
   const paymentData = await req;
 
+  console.log(paymentData);
   try {
     const {
       decodeResponse,
