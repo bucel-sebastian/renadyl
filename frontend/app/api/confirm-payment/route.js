dd6992ce6057b48f4d639b1323d972b62e813b0e
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2023-12-11 15:46:49 +0200

    Update route.js

--- frontend/app/api/confirm-payment/route.js
+++ frontend/app/api/confirm-payment/route.js
@@ -33,11 +33,3 @@ export async function POST(req, res) {
     body: "ok",
   });
 }
-
-
-
-
-ERROR -  ReferenceError: env_key is not defined
-    at POST (/var/task/frontend/.next/server/app/api/confirm-payment/route.js:1:951)
-    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
-    at async /var/task/frontend/node_modules/next/dist/compiled/next-server/app-route.runtime.prod.js:14:39709
