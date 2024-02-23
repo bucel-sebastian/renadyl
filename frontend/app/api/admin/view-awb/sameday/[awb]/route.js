8f6e58052e8c1b81a0b42ee3721bee15a8744fb2
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2024-02-23 15:22:56 +0200

    debug

--- frontend/app/api/admin/view-awb/sameday/[awb]/route.js
+++ frontend/app/api/admin/view-awb/sameday/[awb]/route.js
@@ -5,21 +5,23 @@ export async function GET(req, { params }) {
   const { awb } = params;
   const token = await authSameday();
 
-  // const response = await fetch(
-  //   `${process.env.NEXT_PUBLIC_SAMEDAY_API_DOWNLOAD_AWB_URL}/${awb}`,
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       "X-AUTH-TOKEN": token,
-  //     },
-  //   }
-  // );
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SAMEDAY_API_DOWNLOAD_AWB_URL}/${awb}`,
+    {
+      method: "GET",
+      headers: {
+        "X-AUTH-TOKEN": token,
+      },
+    }
+  );
 
-  // const body = await response.json();
+  const body = await response.blob();
+
+  console.log("blob", blob);
 
   return NextResponse.json({
     status: 200,
-    // body: body,
+    body: body,
     token: token,
   });
 }
