bb4f0ab3b17c4688a5337bc06d535c519047bf07
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2024-02-23 11:08:59 +0200

    Update route.js

--- frontend/app/api/place-order/route.js
+++ frontend/app/api/place-order/route.js
@@ -1,4 +1,5 @@
 import { sendOrderToDatabase } from "@/utils/frontpages/sendOrderToDatabase";
+import { sendOrderPlacedEmail } from "@/utils/nodemailer/OrderPlacedMail";
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
@@ -8,6 +9,26 @@ export async function POST(req, res) {
   try {
     const databaseResponse = await sendOrderToDatabase(requestBody);
     const { getRequest } = require("@/utils/frontpages/netopia/getPaymentData");
+
+    console.log("db resp - ", databaseResponse[0]);
+    console.log("req  - ", requestBody);
+
+    const emailRes = await sendOrderPlacedEmail({
+      email: "",
+      lang: requestBody.locale,
+      order_id: databaseResponse[0].id,
+      order_date: databaseResponse[0].date,
+      order_total: databaseResponse[0].order_total,
+      promo_total: databaseResponse[0].promo_total,
+      vat_total: databaseResponse[0].vat_total,
+      shipping_total: databaseResponse[0].shipping_total,
+      products_total: databaseResponse[0].products_total,
+      shipping: JSON.parse(databaseResponse[0].shipping_details),
+      client_id: databaseResponse[0].client_id,
+      // client_id: "client2081745488",
+      currency: databaseResponse[0].currency,
+      cart: JSON.parse(databaseResponse[0].cart),
+    });
 
     const getPaymentData = await getRequest(
       databaseResponse[0].id,
@@ -19,20 +40,16 @@ export async function POST(req, res) {
 
     responseBody = {
       databaseResponse: await databaseResponse,
+      paymentType: requestBody.checkoutData.payment,
       paymentData: await getPaymentData,
+      emailRes: emailRes,
     };
   } catch (error) {
     console.error("Eroare - ", error);
   }
 
-  return NextResponse.json({
-    status: 200,
-    body: responseBody,
-  });
-}
-export async function GET(req, res) {
-  return NextResponse.json({
-    status: 200,
-    body: "GET Method works!",
-  });
+  // return NextResponse.json({
+  //   status: 200,
+  //   body: responseBody,
+  // });
 }
