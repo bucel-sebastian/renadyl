aaa0aa9e0a76da769f6b48b9bad8f721453d4416
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2024-02-27 22:53:00 +0200

    Fixed title translations for usefull pages

--- frontend/app/[locale]/(otherpages)/useful/privacy-policy/page.js
+++ frontend/app/[locale]/(otherpages)/useful/privacy-policy/page.js
@@ -2,7 +2,7 @@ import { useMessages, useTranslations } from "next-intl";
 import { getTranslator } from "next-intl/server";
 
 export async function generateMetadata({ params: { locale } }) {
-  const t = await getTranslator(locale, "");
+  const t = await getTranslator(locale, "Privacy-policy");
 
   return {
     title: `Renadyl™ -  ${t("page-title")}`,
