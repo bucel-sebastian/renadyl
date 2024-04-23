470e2efdf94268fa7d75557f169b58ac16cd6c8a
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2024-04-23 18:46:43 +0300

    Update confirmPayment.js

--- frontend/utils/frontpages/netopia/confirmPayment.js
+++ frontend/utils/frontpages/netopia/confirmPayment.js
@@ -24,7 +24,10 @@ export const confirmPayment = async (data) => {
   try {
     const decodedPaymentData = await decodeResponse(paymentData);
 
-    console.log("decoded payment - ", decodedPaymentData);
+    console.log(
+      "decoded payment - ",
+      JSON.stringify(decodedPaymentData, null, 2)
+    );
 
     dbResponse = await database.update(
       "renadyl_orders",
@@ -41,4 +44,33 @@ export const confirmPayment = async (data) => {
   }
 
   return dbResponse;
+};
+
+const decoded = {
+  order: {
+    $: { id: "rnd8195052718", timestamp: "1713885842436", type: "card" },
+    signature: "2VZR-GOQH-7A8W-VLRB-UOOW",
+    url: {
+      return: "https://renadyleurope.com/order-placed/",
+      confirm: "https://www.renadyleurope.com/api/v2/confirm",
+    },
+    invoice: { $: [Object], details: "Plata Renadyl", contact_info: [Object] },
+    ipn_cipher: "aes-256-cbc",
+    mobilpay: {
+      $: [Object],
+      action: "confirmed",
+      customer: [Object],
+      purchase: "1765456",
+      original_amount: "477.00",
+      processed_amount: "477.00",
+      current_payment_count: "1",
+      pan_masked: "9****5098",
+      rrn: "9991713",
+      payment_instrument_id: "41679",
+      token_id:
+        "NTkwMTk6SLDsfAv+Pef9LE9mGEY2Yu8B3XgyUl/rn6LdhFgtYGNCoV8ZInF78WpFFl/V5NV3p6B1xyowEaS9HymqG1qGOw==",
+      token_expiration_date: "2027-01-31 23:59:59",
+      error: [Object],
+    },
+  },
 };
